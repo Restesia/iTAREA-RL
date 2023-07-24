33c20b47c4e71efd0ce057ff48445448c451aeed
María Fernández <mariaf@uma.es>
2023-07-24 13:19:47 +0200

    modified:   app/isetter.py

--- app/isetter.py
+++ app/isetter.py
@@ -22,6 +22,18 @@ def set_data_to_transmit(nTask):
     relation = [[0 for _ in range(nTask)] for _ in range(nTask)]
     return relation
 
+# Defining each task that forms the application:
+# 0 - CPU cycles, 
+# 1 - RAM required (mb), 
+# 2 - User (in case of replication for multiple users, 0 by default), 
+# 3 - Minimal transmission (in case of requiring a minimal bandwidtk (e.g., for video streaming in a specific quality)), 
+# 4 - Sensing requirements (e.g., thermometer), 
+# 5 - Peripheral requirements (e.g., camera, GPU), 
+# 6 - Transmit
+# 7 - Exact location (in case the app is located in a specific place (e.g., dispatch 4.1) 'none' by default), 
+# 8 - Type (e.g., sensing mote)
+# 9 - Disk required (Mb) 
+# 10 - Task name
 
 def set_tasks(nTask):
     tasks = [[0 for _ in range(11)] for _ in range(nTask)]
@@ -44,6 +56,23 @@ def set_rtt(nNodes):
     rtt = [[0.01 for _ in range(nNodes)] for _ in range(nNodes)]
     return rtt
 
+# 0 - CPU (cycles per second)
+# 1 - Bandwidth up (bits/s)
+# 2 - Power Upload (Ptx), 
+# 3 - Maximum energy consumption (Watts) 
+# 4 - RAM (Mb)
+# 5 - Importance of saving energy in nodes (real number between 0 and 1). In this case, all nodes has been configured with the same node weighting (1) 
+# 6 - Power Download (Prx), 
+# 7 - Bandwidth Down (bits/s), 
+# 8 - Sensing units (e.g., thermomether), '' if any 
+# 9 - Peripherals (e.g., GPU), '' if any
+# 10 - Type (e.g., sensing mote), 'computing' by default
+# 11- Location (e.g., dispatch 2.30),  
+# 12 - Owner (e,g., CAOSD group), 'public' by default
+# 13 - Communication capacities (e.g., wlan)
+# 14 - Number of cores, 
+# 15 - Percentage of idle energy consumption, 
+# 16 - Percentage of energy consumption in sleeping mode
 
 def set_nodes(nNodes):
     nodes = [[0 for _ in range(17)] for _ in range(nNodes)]
