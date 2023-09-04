c4563c089bb94b5efe5674467b05f0c598a3e6d4
María Fernández <mariaf@uma.es>
2023-09-04 13:34:32 +0200

    modified:   .gitignore 	deleted:    app/__pycache__/ienergy.cpython-310.pyc 	deleted:    app/__pycache__/imain.cpython-310.pyc 	deleted:    app/__pycache__/iprinter.cpython-310.pyc 	deleted:    app/__pycache__/isetter.cpython-310.pyc 	deleted:    app/__pycache__/isolver.cpython-310.pyc 	deleted:    app/__pycache__/proccessor.cpython-310.pyc 	modified:   app/app.py 	modified:   app/templates/enterNodes.html 	modified:   app/templates/enterTasks.html 	modified:   app/templates/index.html

--- app/app.py
+++ app/app.py
@@ -17,12 +17,14 @@ route = 'imain.py' #TRABAJO
 #route = 'app_metrics/app/imain.py'
 #route = 'app/imain.py' #PERSONAL
 
-@app.route("/getNodesFromParameters")
-def getNodesFromParameters():
+
+#---------LOAD HTML TEMPLATES---------
+@app.route("/loadNodesTemplate")
+def loadNodesTemplate():
     return render_template("enterNodes.html",  nodes_list=nodes_list, zip_lists=zip_lists)
 
-@app.route("/getTasksFromParameters")
-def getTasksFromParameters():
+@app.route("/loadTasksTemplate")
+def loadTasksTemplate():
     return render_template("enterTasks.html", tasks_list=tasks_list, zip_lists=zip_lists)
 
 @app.route("/readFile")
@@ -34,37 +36,7 @@ def printValues():
     output = loadData(json.dumps(nodes_list), json.dumps(tasks_list))
     return render_template('printValues.html', result=output, determine_back_route=determine_back_route)
 
-
-def loadData(nodes_json, tasks_json):
-    output=""
-    try:
-        output = subprocess.check_output(['python', route, nodes_json, tasks_json], text=True)
-        if not output.strip(): 
-            output = "No result"
-    except subprocess.CalledProcessError as e:
-        print("Error executing imain.py:", e)
-        print("Error output:", e.output)
-    return output
-
-@app.route('/processAndPrint', methods=['POST'])
-def processAndPrint():
-    
-    nodes_json=""
-    tasks_json=""
-    output=""
-
-    if request.method == 'POST':
-        file = request.files['file_input']
-        if file:
-            try:
-                nodes_json, tasks_json = proccess(file)
-                output = loadData(nodes_json, tasks_json)
-            except json.JSONDecodeError as json_error:
-                error_message = f"Error decoding JSON: {json_error}"
-                return render_template('printValues.html', result=error_message, determine_back_route=determine_back_route)
-            except Exception as e:
-                return render_template('printValues.html', result=str(e), determine_back_route=determine_back_route) 
-    return render_template('printValues.html', result=output, determine_back_route=determine_back_route)
+#---------ADD AND DELETE---------
 
 @app.route('/add_new_node', methods=['POST'])
 def add_new_node():
@@ -91,7 +63,7 @@ def add_new_node():
     }
     nodes_list.append(new_object)
     #printAll(nodes_list)
-    return redirect(url_for('getNodesFromParameters'))
+    return redirect(url_for('loadNodesTemplate'))
 
 @app.route('/add_new_task', methods=['POST'])
 def add_new_task():
@@ -110,66 +82,55 @@ def add_new_task():
     }
     tasks_list.append(new_object)
     #printAll(tasks_list)
-    return redirect(url_for('getTasksFromParameters'))
-
-def printAll(list):
-    for elem in list:
-        print(elem)
-
-def zip_lists(a, b):
-    return zip(a, b)
-
-def determine_back_route():
-    current_path = request.referrer
-    if current_path.endswith('getTasksFromParameters'):
-        return 'getTasksFromParameters'  # Nombre de la función para entertasks.html
-    elif current_path.endswith('readFile'):
-        return 'readFile'  # Nombre de la función para readfile.html
-    else:
-        return 'index'  # Nombre de la función para la página de inicio
+    return redirect(url_for('loadTasksTemplate'))
 
 @app.route('/delete_node', methods=['POST'], )
 def delete_node():
-    
-    index = request.form['index']
-    print(index)
-    if index is not None and index.isdigit():
-        index = int(index)
-        print(index)
-        if 0 <= index < len(nodes_list):
-            del nodes_list[index]
-    return redirect('/getNodesFromParameters')
+    delete_object(nodes_list)
+    return redirect('/loadNodesTemplate')
 
 @app.route('/delete_task', methods=['POST'])
 def delete_task():
-    index = request.form['index']
-    print(index)
-    if index is not None and index.isdigit():
-        index = int(index)
-        print(index)
-        if 0 <= index < len(tasks_list):
-            del tasks_list[index]
-    return redirect('/getTasksFromParameters')
+    delete_object(tasks_list)
+    return redirect('/loadTasksTemplate')
+
+#---------LOAD MORE INFO---------
+
+@app.route('/processAndPrint', methods=['POST'])
+def processAndPrint():
+    
+    nodes_json=""
+    tasks_json=""
+    output=""
+
+    if request.method == 'POST':
+        file = request.files['file_input']
+        if file:
+            try:
+                nodes_json, tasks_json = proccess(file)
+                output = loadData(nodes_json, tasks_json)
+            except json.JSONDecodeError as json_error:
+                error_message = f"Error decoding JSON: {json_error}"
+                return render_template('printValues.html', result=error_message, determine_back_route=determine_back_route)
+            except Exception as e:
+                return render_template('printValues.html', result=str(e), determine_back_route=determine_back_route) 
+    return render_template('printValues.html', result=output, determine_back_route=determine_back_route)
 
 @app.route("/saveResult", methods=["POST"])
 def saveResult():
+
     try:
         now = datetime.datetime.now()
         date_string = now.strftime("%d-%m-%y_%H.%M")
         filename = f"{date_string}.txt"
         location = request.form.get("location")
 
-        if not location:
-            raise ValueError("La ubicación no puede estar vacía")
-
+        if not location:    raise ValueError("La ubicación no puede estar vacía")
         filepath = os.path.join(location, filename)
-
         result = request.form.get("result", "")  # Obtener el contenido del resultado
-
-        with open(filepath, "w") as file:
-            file.write(result)
-
+        with open(filepath, "w") as file:   file.write(result)
         message = f"El resultado se ha guardado en {filepath}"
+
     except FileNotFoundError as e:
         message = f"Error al guardar el resultado: {str(e)}"
     except PermissionError as e:
@@ -181,19 +142,49 @@ def saveResult():
 
     return message
 
+#---------AUX FUNCTIONS---------
+
+def loadData(nodes_json, tasks_json):
+    output=""
+    try:
+        output = subprocess.check_output(['python', route, nodes_json, tasks_json], text=True)
+        if not output.strip(): 
+            output = "No result"
+    except subprocess.CalledProcessError as e:
+        print("Error executing imain.py:", e)
+        print("Error output:", e.output)
+    return output
+
+def zip_lists(a, b):     return zip(a, b)
+
+def printAll(list):
+    for elem in list:
+        print(elem)
+
+def determine_back_route():
+    current_path = request.referrer
+    if current_path.endswith('loadTasksTemplate'):
+        return 'loadTasksTemplate'  # Nombre de la función para entertasks.html
+    elif current_path.endswith('readFile'):
+        return 'readFile'  # Nombre de la función para readfile.html
+    else:
+        return 'index'  # Nombre de la función para la página de inicio
+    
+def delete_object(this_list):
+    index = request.form['index']
+    print(index)
+    if index is not None and index.isdigit():
+        index = int(index)
+        print(index)
+        if 0 <= index < len(this_list):
+            del this_list[index]
+    
+#---------MAIN FUNCTIONS---------
+
 @app.route('/')
 def index():
-
-    #example_list = ['element1','element2','element3']
-    #data={
-    #    'title':'Metrics to energy app',
-    #    'hi':"Hello!",
-    #    'example_list':example_list,
-    #    'list_length':len(example_list)
-    #}
-
-    return render_template('index.html' #, data=data
-                           )
+    return render_template('index.html')
+    #return render_template('index.html', data=data)
 
 if __name__=='__main__':
     app.run(debug=True, port=5000) #removable options inside the function
