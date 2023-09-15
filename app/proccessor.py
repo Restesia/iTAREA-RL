2daa3608acf9f398c54b252f2dc7ab2e608c27f2
María Fernández <mariaf@uma.es>
2023-09-15 10:54:02 +0200

    new file:   app/kubernetesExperiments/pruebas.py 	modified:   app/proccessor.py

--- app/proccessor.py
+++ app/proccessor.py
@@ -9,13 +9,13 @@ def proccess(file):
 #---------------------------------------AUX METHODS---------------------------------------
 
 def getContentAndExtension(file):
-    file_content_with_breaks = ((file.read()).decode('utf-8'))
-    file_content = ''.join(file_content_with_breaks.splitlines())   #STRING
+    file_content = ((file.read()).decode('utf-8'))
+    #file_content = ''.join(file_content.splitlines())   #STRING
     file_extension = (file.filename).rsplit('.', 1)[-1].lower()     #STRING
     return file_content, file_extension
 
 def classifyAndGetData(file_content, file_extension):
-    if(file_extension=="json"): 
+    if(file_extension=="json"):
         return json_read(file_content)    
     if(file_extension=="txt"):
         return txt_read(file_content)
@@ -25,29 +25,22 @@ def classifyAndGetData(file_content, file_extension):
 
 #---------------------------------------READERS---------------------------------------
 
- #FALLO: DEVUELVE VACíAS LAS LISTAS nodes_json, tasks_json
 def json_read(file_content):
-    nodes_json = []
-    tasks_json = []
-
-    lines = file_content.strip().split('\n')
-    
-    for line_number, line in enumerate(lines, start=1):
-        data = json.loads(line)
-        
-        #FALLO: ¿ENTRA EN ESTOS IF?
-        if "name" in data and "cpu" in data:
-            nodes_json.append(data)
-            print("NODES JSON: " , nodes_json)
-        elif "taskname" in data and "cpucycles" in data:
-            tasks_json.append(data)
-            print("TASKS JSON: " , nodes_json)
-
-    nodes_json_string = "[" + ", ".join([json.dumps(item) for item in nodes_json]) + "]"
-    tasks_json_string = "[" + ", ".join([json.dumps(item) for item in tasks_json]) + "]"
-
-    return nodes_json_string, tasks_json_string
-
+    nodes = []
+    tasks = []
+    try:
+        data = json.loads(file_content)
+        for item in data:
+            if 'name' in item:
+                nodes.append(item)
+            elif 'taskname' in item:
+                tasks.append(item)
+        nodes_json_string = json.dumps(nodes)
+        tasks_json_string = json.dumps(tasks)
+        return nodes_json_string, tasks_json_string
+    except json.JSONDecodeError as e:
+        print("Error al decodificar el JSON:", str(e))
+        return None, None
 
 def txt_read(file_content):
     file_json = json.dumps(file_content)
