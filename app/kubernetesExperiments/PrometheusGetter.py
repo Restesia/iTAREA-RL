b245e9a3ccb687f29714cc858cc46f2b20812395
María Fernández <mariaf@uma.es>
2023-09-18 15:30:22 +0200

    modified:   app/kubernetesExperiments/PrometheusGetter.py 	deleted:    app/kubernetesExperiments/__pycache__/cpuBwGetter.cpython-38.pyc 	modified:   app/templates/index.html

--- app/kubernetesExperiments/PrometheusGetter.py
+++ app/kubernetesExperiments/PrometheusGetter.py
@@ -4,10 +4,12 @@ from prometheus_api_client import PrometheusConnect
 prom = PrometheusConnect(url="http://localhost:30000")
 
 # Define la consulta para obtener las métricas de uso de CPU de un contenedor específico
-query = 'container_cpu_usage_seconds_total{namespace="default"}'
+query1 = 'container_cpu_usage_seconds_total{namespace="default"}'
+query2 = 'machine_nvm_avg_power_budget_watts'
 
 # Ejecuta la consulta
-results = prom.custom_query(query)
+results = prom.custom_query(query1)
+results = prom.custom_query(query2)
 
 # El resultado será un diccionario con las métricas de uso de CPU
 cpu_usage = float(results[0]['value'][1])
