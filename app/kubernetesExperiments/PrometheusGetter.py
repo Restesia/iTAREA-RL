abdb5b90fa5921f156b7b7277a707e94633d38e7
María Fernández <mariaf@uma.es>
2023-09-18 16:09:59 +0200

    modified:   app/kubernetesExperiments/PrometheusGetter.py

--- app/kubernetesExperiments/PrometheusGetter.py
+++ app/kubernetesExperiments/PrometheusGetter.py
@@ -1,16 +1,12 @@
 from prometheus_api_client import PrometheusConnect
 
-# Conecta con el servidor Prometheus
 prom = PrometheusConnect(url="http://localhost:30000")
 
-# Define la consulta para obtener las métricas de uso de CPU de un contenedor específico
-query1 = 'container_cpu_usage_seconds_total{namespace="default"}'
-query2 = 'machine_nvm_avg_power_budget_watts'
+def getMetricFromQuery(info, query):
+    result = prom.custom_query(query)
+    value = float(result[0]['value'][1])
+    print(info,': ' ,value)
+    return value
 
-# Ejecuta la consulta
-results = prom.custom_query(query1)
-results = prom.custom_query(query2)
-
-# El resultado será un diccionario con las métricas de uso de CPU
-cpu_usage = float(results[0]['value'][1])
-print("Uso de CPU (decimal):", cpu_usage)
+getMetricFromQuery('Uso de CPU (decimal)', 'container_cpu_usage_seconds_total{namespace="default"}')
+getMetricFromQuery('Average power budget (watts)', 'machine_nvm_avg_power_budget_watts')
